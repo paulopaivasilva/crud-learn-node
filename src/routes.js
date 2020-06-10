db9fa93b24e98f773364321d4b353b7073b63675
Paulo Henrique Paiva <paulohenriquepaiva@ymail.com>
2020-06-10 07:05:35 -0300

    Update routes.js
    - Nova rota com teste em conexão com o banco.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    mongoose.connect('mongodb://admin:admin2020@ds115340.mlab.com:15340/heroku_70kzsjw3');
+    const connect = mongoose.connection
+    connect.once('open', () => {
+        res.json({ data: 'deu certo' });
+    }).on('error', () => {
+        res.json({ data: 'erro' })
+    })
+
+})
 
 routes.post('/login', (req, res) => {
     return res.json(req.body);
@@ -8,6 +20,7 @@ routes.post('/login', (req, res) => {
 
 routes.get('/user/:id', (req, res) => {
     return res.json(req.params.id);
+    
 })
 
 module.exports = routes;
